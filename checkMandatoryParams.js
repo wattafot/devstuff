d82c19e8cc4633550f485d087e589a7a5d47ecb7
wattafot <eslgerrrda@googlemail.com>
2016-08-17 15:20:29 +0200

    Update checkMandatoryParams.js

--- checkMandatoryParams.js
+++ checkMandatoryParams.js
@@ -1,7 +1,7 @@
-	function checkMandatoryParams(target, referene) {
+	function checkMandatoryParams(target, reference) {
 		const targetParams = Object.keys(target)
 
-		refernce.forEach(key => {
+		reference.forEach(key => {
 			if(targetParams.includes(key) === false)
 				throw err('missing parameter: '+key)
 		})
